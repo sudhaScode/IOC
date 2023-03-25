3ff4abc41f6c113993ccc8a7d01154852ce6988d
sudhaScode <sunriser7995@gmail.com>
2023-03-25 21:28:33 +0530

    project

---
+++ src/main/java/demo/concept/inversionofcontrol/InversionofcontrolApplication.java
@@ -0,0 +1,13 @@
+package demo.concept.inversionofcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InversionofcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InversionofcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/concept/inversionofcontrol/InversionofcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.concept.inversionofcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InversionofcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
